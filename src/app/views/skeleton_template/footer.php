948000e6ee40558c586725dda30830845d35ae53
Adarsh Sanjeev <adarshsanjeev@gmail.com>
2016-10-08 08:55:43 +0000

    Removed footer background

--- src/app/views/skeleton_template/footer.php
+++ src/app/views/skeleton_template/footer.php
@@ -14,105 +14,46 @@ if (empty($is_ajax)):
             }
         }
     }
-    if (!isset($page_slug)) {
-        $page_slug = "static";
-    }
-    $primary_nav_link = function ($name, $display_name, $image) use ($page_slug) {
-?>
-        <a href="<?= locale_base_url() . $name ?>/" data-href="<?= $name ?>" class="primary-nav-link <?= $name ?><?= $name == $page_slug ? ' open' : '' ?>">
-            <img src="<?= base_url() ?>static/images/<?= $image ?>">
-            <div class="title"><?= $display_name ?></div>
-        </a>
+if (!isset($page_slug)) {
+    $page_slug = "static";
+}
+$primary_nav_link = function ($name, $display_name, $image) use ($page_slug) {
+    ?>
+    <a href="<?= locale_base_url() . $name ?>/" data-href="<?= $name ?>" class="primary-nav-link <?= $name ?><?= $name == $page_slug ? ' open' : '' ?>">
+    <img src="<?= base_url() ?>static/images/<?= $image ?>">
+    <div class="title"><?= $display_name ?></div>
+    </a>
 <?php
-    }
-?>
-    </div>
-    <nav>
-        <?php if (isset($categorised_event)): ?>
-        <div class="crystal-ball">
-            <div class="ball-title" id="cards-container"><?= __('Events') ?> </div>
-            <ul class="events-nav">
-                <?php foreach ($categorised_event as $category => $events): ?>
-                    <li>
-                        <a class="events-nav-button" href="#" data-href="<?= $category ?>">
-                            <i class="<?= $events[0]['data']['icon'] ?>"></i>
-                        </a>
-                    </li>
-                <?php endforeach; ?>
-            </ul>
-        </div>
-        <div class="events-nav-cum-tooltip">
-            <?php foreach ($categorised_event as $category => $events): ?>
-                <div class="nav-cum-tooltip-dummy-target cat-<?= $category ?>">
-                    <?php
-                        $category_name = "";
-                        $events_sub_nav_home = [];
-                        $events_sub_nav = [];
-                        foreach ($events as $event) {
-                            if (substr($event['path'], -6) === 'index/') {
-                                $category_name = $event['data']['name'];
-                                $events_sub_nav_home = [
-                                    'path'=> substr($event['path'], 1, -6),
-                                    'name'=> __('Home')
-                                ];
-                            } else {
-                                array_push($events_sub_nav, [
-                                    'path'=> substr($event['path'], 1),
-                                    'name'=> $event['data']['name']
-                                ]);
-                            }
-                        }
-                        usort($events_sub_nav, function($a, $b) {
-                            return $a['name'] - $b['name'];
-                        });
-
-                        // Localize now
-                        foreach ($events_sub_nav as $key => $event) {
-                            $events_sub_nav[$key]['name'] = __($event['name']);
-                        }
-
-                        // Add `Home` at the begining
-                        array_unshift($events_sub_nav, $events_sub_nav_home);
-                    ?>
-                    <div class="nav-cum-tooltip">
-                        <div class="nav-title"><?= __($category_name) ?></div>
-                        <ul class="events-sub-nav">
-                            <?php foreach ($events_sub_nav as $event): ?>
-                                <li><a href="<?= locale_base_url() . $event['path'] ?>"><?= $event['name'] ?></a></li>
-                            <?php endforeach; ?>
-                        </ul>
-                        <div class="triangle-back"></div>
-                        <div class="triangle-front"></div>
-                    </div>
-                </div>
-            <?php endforeach; ?>
-        </div>
-        <?php endif; ?>
-<!--
-        <ul class="primary-nav left">
-            <li>
+}
+    ?>
+</div>
+<nav>
+<div id="cards-container"/>
+    <!--
+    <ul class="primary-nav left">
+    <li>
                 <?php $primary_nav_link('about', __('About'), 'dragon8.png'); ?>
-            </li>
-            <li>
+    </li>
+    <li>
                 <?php $primary_nav_link('gallery', __('Gallery'), 'dragon2.png'); ?>
-            </li>
-            <li>
+    </li>
+    <li>
                 <?php $primary_nav_link('schedule', __('Schedule'), 'dragon7.png'); ?>
-            </li>
-        </ul>
-        <ul class="primary-nav right">
-            <li>
+    </li>
+    </ul>
+    <ul class="primary-nav right">
+    <li>
                 <?php $primary_nav_link('sponsors', __('Sponsors'), 'dragon5.png'); ?>
-            </li>
-            <li>
+    </li>
+    <li>
                 <?php $primary_nav_link('team', __('Team'), 'dragon3.png'); ?>
-            </li>
-            <li>
+    </li>
+    <li>
                 <?php $primary_nav_link('contact', __('Contact'), 'dragon6.png'); ?>
-            </li>
-        </ul>
+    </li>
+    </ul>
     </nav>
--->
+    -->
     <link href="<?= base_url() ?>static/styles/card-menu.css" rel="stylesheet">
     <script src="<?= base_url() ?>static/scripts/common.js" charset="utf-8"></script>
     <script src="<?= base_url() ?>static/scripts/deck.js" charset="utf-8"></script>
@@ -122,6 +63,6 @@ if (empty($is_ajax)):
     <script src="<?= base_url() ?>static/scripts/navigation.js?v=4" charset="utf-8"></script>
     <?php endif; ?>
     <?php $this->load_fragment('google_analytics'); ?>
-</body>
-</html>
+    </body>
+    </html>
 <?php endif;
